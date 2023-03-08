7f6dd778e11091eda978545acfc57165706b0a6c
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2023-03-08 13:44:46 -0500

    Revert "add multiple targets (Pi4, Pi3, TX1, and RK3399) (#1)" (#3)
    This reverts commit 65cea6daac3e015bd87735c5bc2b89e3ca9848b3.

--- create-deb.sh
+++ create-deb.sh
@@ -41,52 +41,37 @@ fi
 echo "box86 is not the latest version, compiling now."
 echo $commit > $DIRECTORY/commit.txt
 echo "Wrote commit to commit.txt file for use during the next compilation."
+mkdir build && cd build
+cmake .. -DCMAKE_BUILD_TYPE=RelWithDebInfo -DRPI4=1 || error "Failed to run cmake."
+make -j4 || error "Failed to run make."
 
-targets=(ARM64 RPI4ARM64 RPI3ARM64 TEGRAX1 RK3399)
+function get-box86-version() {
+	if [[ $1 == "ver" ]]; then
+		export BOX86VER="$(./box86 -v | cut -c21-25)"
+	elif [[ $1 == "commit" ]]; then
+		export BOX86COMMIT="$commit"
+	fi
+}
 
-for target in ${targets[@]}; do
+get-box86-version ver  || error "Failed to get box86 version!"
+get-box86-version commit || error "Failed to get box86 commit!"
+DEBVER="$(echo "$BOX86VER+$(date +"%F" | sed 's/-//g').$BOX86COMMIT")" || error "Failed to set debver variable."
 
-  cd $DIRECTORY/box86
-  sudo rm -rf build && mkdir build && cd build || error "Could not move to build directory"
-  cmake .. -D$target=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo || error "Failed to run cmake."
-  make -j4 || error "Failed to run make."
+mkdir doc-pak || error "Failed to create doc-pak dir."
+cp ../docs/README.md ./doc-pak || warning "Failed to add README to docs"
+cp ../docs/CHANGELOG.md ./doc-pak || error "Failed to add CHANGELOG to docs"
+cp ../docs/USAGE.md ./doc-pak || error "Failed to add USAGE to docs"
+cp ../docs/X86WINE.md ./doc-pak || error "Failed to add X86WINE to docs"
+cp ../LICENSE ./doc-pak || error "Failed to add LICENSE to docs"
+echo "Box86 lets you run x86 Linux programs (such as games) on non-x86 Linux systems, like ARM (host system needs to be 32bit little-endian)">description-pak || error "Failed to create description-pak."
+echo "#!/bin/bash
+echo 'Restarting systemd-binfmt...'
+systemctl restart systemd-binfmt || true" > postinstall-pak || error "Failed to create postinstall-pak!"
 
-  function get-box86-version() {
-    if [[ $1 == "ver" ]]; then
-      export BOX86VER="$(./box86 -v | cut -c21-25)"
-    elif [[ $1 == "commit" ]]; then
-      export BOX86COMMIT="$commit"
-    fi
-  }
+sudo checkinstall -y -D --pkgversion="$DEBVER" --arch="armhf" --provides="box86" --conflicts="qemu-user-static" --pkgname="box86" --install="no" make install || error "Checkinstall failed to create a deb package."
 
-  get-box86-version ver  || error "Failed to get box86 version!"
-  get-box86-version commit || error "Failed to get box86 commit!"
-  DEBVER="$(echo "$BOX86VER+$(date +"%F" | sed 's/-//g').$BOX86COMMIT")" || error "Failed to set debver variable."
-
-  mkdir doc-pak || error "Failed to create doc-pak dir."
-  cp ../docs/README.md ./doc-pak || warning "Failed to add README to docs"
-  cp ../docs/CHANGELOG.md ./doc-pak || error "Failed to add CHANGELOG to docs"
-  cp ../docs/USAGE.md ./doc-pak || error "Failed to add USAGE to docs"
-  cp ../docs/X86WINE.md ./doc-pak || error "Failed to add X86WINE to docs"
-  cp ../LICENSE ./doc-pak || error "Failed to add LICENSE to docs"
-  echo "Box86 lets you run x86 Linux programs (such as games) on non-x86 Linux systems, like ARM (host system needs to be 32bit little-endian)">description-pak || error "Failed to create description-pak."
-  echo "#!/bin/bash
-  echo 'Restarting systemd-binfmt...'
-  systemctl restart systemd-binfmt || true" > postinstall-pak || error "Failed to create postinstall-pak!"
-
-  # use the pi4 target as the default box86 package
-  if [[ $target == "RPI4ARM64" ]]; then
-    sudo checkinstall -y -D --pkgversion="$DEBVER" --arch="armhf" --provides="box86" --conflicts="qemu-user-static" --pkgname="box86" --install="no" make install || error "Checkinstall failed to create a deb package."
-  elif [[ $target == "ARM64" ]]; then
-    sudo checkinstall -y -D --pkgversion="$DEBVER" --arch="armhf" --provides="box86" --conflicts="qemu-user-static" --pkgname="box86-generic-arm" --install="no" make install || error "Checkinstall failed to create a deb package."
-  else
-    sudo checkinstall -y -D --pkgversion="$DEBVER" --arch="armhf" --provides="box86" --conflicts="qemu-user-static" --pkgname="box86-$target" --install="no" make install || error "Checkinstall failed to create a deb package."
-  fi
-
-  cd $DIRECTORY
-  mv box86/build/*.deb ./debian/ || error "Failed to move deb to debian folder."
-
-done
+cd $DIRECTORY
+mv box86/build/*.deb ./debian/ || error "Failed to move deb to debian folder."
 
 rm -rf $DIRECTORY/box86
 
