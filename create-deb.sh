86b64b85295a27383747232af83f67885fcb7765
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2023-07-14 18:56:13 -0400

    fix cmake build commands

--- create-deb.sh
+++ create-deb.sh
@@ -49,9 +49,9 @@ for target in ${targets[@]}; do
   cd $DIRECTORY/box86
   sudo rm -rf build && mkdir build && cd build || error "Could not move to build directory"
   if [[ $target == "ANDROID" ]]; then
-    cmake .. -DARM64=1 -DBAD_SIGNAL=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=gcc-8 || error "Failed to run cmake."
+    cmake .. -DARM64=1 -DBAD_SIGNAL=ON -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=gcc-9 || error "Failed to run cmake."
   else
-    cmake .. -D$target=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=gcc-8 || error "Failed to run cmake."
+    cmake .. -D$target=1 -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_C_COMPILER=gcc-9 || error "Failed to run cmake."
   fi
   make -j4 || error "Failed to run make."
 
