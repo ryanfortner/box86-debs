2005e8a83258730c11c7e3a16933c1f3cb6910cd
Ryan Fortner <71036629+ryanfortner@users.noreply.github.com>
2025-04-21 10:43:34 -0400

    Remove pkg-config dependency...

--- docker-script.sh
+++ docker-script.sh
@@ -7,7 +7,7 @@ sudo wget curl build-essential jq autoconf automake \
 pkg-config ca-certificates rpm apt-utils \
 python3 make gettext pinentry-tty devscripts dpkg-dev \
 gcc-11 g++-11 \
-gcc-11-arm-linux-gnueabihf g++-11-arm-linux-gnueabihf binutils-arm-linux-gnueabihf pkg-config-arm-linux-gnueabihf
+gcc-11-arm-linux-gnueabihf g++-11-arm-linux-gnueabihf binutils-arm-linux-gnueabihf
 sudo dpkg --add-architecture armhf
 sudo apt update
 sudo apt install -y libc6:armhf
